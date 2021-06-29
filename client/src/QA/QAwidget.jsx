01889317bc14f60797e87f9c51ca2f58f26da46e
Julian Thong <julianzthong@gmail.com>
2021-06-28 19:17:54 -0700

    Andys adjustments to the QA section

--- client/src/QA/QAwidget.jsx
+++ client/src/QA/QAwidget.jsx
@@ -1,33 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 import Search from './QAcomponents/Search.jsx';
 import QuestionList from './QAcomponents/QuestionList.jsx';
 import AddQuestion from './QAcomponents/AddQuestion.jsx';
-
 const QAwidget = ( { productId } ) => {
-  const [questions, setQuestions] = useState([]);
   const [searchInput, setSearchInput] = useState(null);
-  const [displayQuestions, setDisplayQuestions] = useState(4);
   const [openModal, setOpenModal] = useState(false);
-
-  useEffect(() => {
-    axios.get('/qa/questions', {params: { productId: productId }})
-      .then(response => {
-        setQuestions(response.data.results
-          .sort((a, b) => a.helpfulness - b.helpfulness));
-      })
-      .catch(err => console.log(err));
-  }, [productId]);
-
   const handleSearch = (searchInput) => {
     searchInput.length > 2
       ? setSearchInput(searchInput)
       : setSearchInput('');
   };
-
   const handleModalOpen = () => setOpenModal(true);
   const handleModalClose = () => setOpenModal(false);
-
   return (
     <>
       <div className='qa-widget'>
@@ -36,13 +20,10 @@ const QAwidget = ( { productId } ) => {
           handleSearch={handleSearch}
           searchInput={searchInput}
         />
-        {questions !== undefined && Object.keys(questions).length !== 0 ?
-          <QuestionList
-            productId={productId}
-            searchInput={searchInput}
-            questions={questions}
-          />
-          : null}
+        <QuestionList
+          productId={productId}
+          searchInput={searchInput}
+        />
         <button className='add-question-button' onClick={handleModalOpen}>
           ADD A QUESTION +
         </button>
@@ -55,5 +36,4 @@ const QAwidget = ( { productId } ) => {
     </>
   );
 };
-
 export default QAwidget;
